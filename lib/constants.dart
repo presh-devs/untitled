04a4e5bf5c582bf57f2f173459cc3cfb61985470
Precious <olwafemiprecious2017@gmail.com>
2022-10-16 17:43:40 +0100

    update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
 
 class ApiConstants {
-  static String baseUrl =
-      'https://fashionbiz.org/1eca16c1127fcaf8266a3ae56dffb540f5eaac9f-889fe0e508bf0365111cc95114e29263-88061744';
+  static String baseUrl = 'https://fashionbiz.org';
   static String login = '/api/auth';
   static String updateUserData = '/api/update-user-data?access token=';
 }
@@ -13,3 +12,4 @@ const kAccentColor = Color(
 );
 
 // 0xff9098B1
+// /1eca16c1127fcaf8266a3ae56dffb540f5eaac9f-889fe0e508bf0365111cc95114e29263-88061744
